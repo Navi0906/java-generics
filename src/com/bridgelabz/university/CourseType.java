c7238291efdbf157a667eb69d22e6c7b23ba6dc5
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-27 23:14:35 +0530

    Naam Simran Singh Add: Course type function

---
+++ src/com/bridgelabz/university/CourseType.java
@@ -0,0 +1,26 @@
+package src.com.bridgelabz.university;
+
+public abstract class CourseType {
+    private String courseName;
+    private String department;
+
+    public CourseType(String courseName, String department) {
+        this.courseName = courseName;
+        this.department = department;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public abstract String getEvaluationMethod();
+
+    @Override
+    public String toString() {
+        return courseName + " (" + department + ") - " + getEvaluationMethod();
+    }
+}
