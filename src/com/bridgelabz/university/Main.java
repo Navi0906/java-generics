ac641e1b93778c513ed00f8766696fbd51679309
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-27 23:19:03 +0530

    Naam Simran Singh Add: Main function

---
+++ src/com/bridgelabz/university/Main.java
@@ -0,0 +1,25 @@
+package src.com.bridgelabz.university;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ExamCourse mathCourse = new ExamCourse("Mathematics", "Science");
+        AssignmentCourse historyCourse = new AssignmentCourse("History", "Arts");
+        ResearchCourse aiCourse = new ResearchCourse("Artificial Intelligence", "Computer Science");
+
+
+        Course<ExamCourse> math = new Course<>(mathCourse, 4);
+        Course<AssignmentCourse> history = new Course<>(historyCourse, 3);
+        Course<ResearchCourse> aiResearch = new Course<>(aiCourse, 5);
+
+
+        CourseCatalog catalog = new CourseCatalog();
+        catalog.addCourse(math);
+        catalog.addCourse(history);
+        catalog.addCourse(aiResearch);
+
+
+        System.out.println("University Course Catalog:");
+        catalog.showCourses();
+    }
+}
